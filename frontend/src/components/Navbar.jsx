18111821056af0effab1a8fa4a0989f718b96f9d
asjadahamed77 <asjadahamed77@gmail.com>
2024-10-20 19:51:04 +0530

    Updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { NavLink } from "react-router-dom";
+import { NavLink,Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between lg:px-12 sm:px-6 px-4 py-2 bg-secondaryColor">
-      <img className="w-32" src={logo} alt="" />
+      <Link to={'/'}><img className="w-32" src={logo} alt="" /></Link>
       <div className="text-white text-base hidden  sm:flex items-center  lg:gap-12 md:gap-6 sm:gap-4">
-        <NavLink>Home</NavLink>
-        <NavLink>About</NavLink>
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/about'}>About</NavLink>
         <NavLink to={'/services'}>Services</NavLink>
-        <NavLink>Contact us</NavLink>
+        <NavLink to={'/contact-us'}>Contact us</NavLink>
         <button className="bg-mainColor px-8 py-2 rounded-full text-sm">Log In</button>
       </div>
     </div>
